aac2ac3f731c3451625a2a4eeac391f7acce4fe2
Ivan Zhuravlev <izhuravlev27@gmail.com>
2023-09-23 22:24:07 +0500

    add app info

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+	QCoreApplication::setOrganizationName("IZhuravlev");
+	QCoreApplication::setApplicationName("tomato");
+	// QCoreApplication::setApplicationVersion("1.1.0");
+
     MainForm mainForm;
     mainForm.show();
     return app.exec();
